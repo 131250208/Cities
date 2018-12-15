12ef3375b90b87e5b11819ac00a4478c5752171b
MaskAdam <wychengpublic@163.com>
2018-12-15 21:24:19 +0800

    -

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/131250208/Cities'
 EMAIL = 'wychengpublic@163.com'
 AUTHOR = 'Andy Wong'
 REQUIRES_PYTHON = '>=3.6.0'
-VERSION = 0.1
+VERSION = 0.4
 
 # What packages are required for this module to be executed?
 REQUIRED = [
