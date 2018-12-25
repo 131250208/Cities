88add36db0eeccdb261f3ffceb30250237087dab
MaskAdam <wychengpublic@163.com>
2018-12-25 17:38:56 +0800

    change version num

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/131250208/Cities'
 EMAIL = 'wychengpublic@163.com'
 AUTHOR = 'Andy Wong'
 REQUIRES_PYTHON = '>=3.5.0'
-VERSION = 0.9
+VERSION = 1.0
 
 # What packages are required for this module to be executed?
 REQUIRED = [
